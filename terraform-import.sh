df25f39ec4b4a6e6b26220d6bf0be4b5e382baf9
Edilberto Mario Rodríguez Fontalvo <edimarod@outlook.es>
2025-05-30 04:19:16 +0000

    feat: resources updated

--- terraform-import.sh
+++ terraform-import.sh
@@ -2,13 +2,14 @@
 
 set -e
 
+account_id=$(aws sts get-caller-identity --query Account --output text)
 imports=(
   "aws_s3_bucket.source-data-bucket source-data-bucket-6i2caq"
   "aws_s3_bucket.target-data-bucket target-data-bucket-6i2caq"
   "aws_s3_bucket.code-bucket code-bucket-6i2caq"
   "aws_iam_role.glue_service_role glue_service_role"
-  "aws_iam_role_policy.glue_service_role_policy glue_service_role/glue_service_role_policy"
-  "aws_glue_catalog_database.org_report_database org-report"
+  "aws_iam_role_policy.glue_service_role_policy glue_service_role:glue_service_role_policy"
+  "aws_glue_catalog_database.org_report_database ${account_id}:org-report"
   "aws_glue_crawler.org_report_crawler org-report-crawler"
   "aws_glue_trigger.org_report_trigger org-report-trigger"
   "aws_glue_job.glue_job glue-job"
@@ -20,7 +21,7 @@ imports=(
   # "aws_security_group.redshift_sg <security_group_id>" 
   "aws_redshift_cluster.main redshift-cluster"
   "aws_iam_role.step_function_role step-function-role"
-  "aws_iam_role_policy.step_function_policy step-function-role/step-function-policy"
+  "aws_iam_role_policy.step_function_policy step-function-role:step-function-policy"
   "aws_sfn_state_machine.etl_workflow ETLWorkflow"
 )
 
