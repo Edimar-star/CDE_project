9e14af51adba7330d988ec2f77088cdaab94d59f
Edilberto Mario Rodríguez Fontalvo <edimarod@outlook.es>
2025-05-29 19:47:28 -0500

    fix: Update step_functions.tf

--- step_functions.tf
+++ step_functions.tf
@@ -38,7 +38,7 @@ resource "aws_sfn_state_machine" "etl_workflow" {
   role_arn = aws_iam_role.step_function_role.arn
 
   definition = jsonencode({
-    StartAt: "LambdaRun1",
+    StartAt: "LambdaRun",
     States: {
       LambdaRun: {
         Type: "Task",
