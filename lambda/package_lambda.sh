66ccbadd3cd16958274cf16f2661a83f5a992bc1
Edilberto Mario Rodríguez Fontalvo <edimarod@outlook.es>
2025-06-01 15:39:28 +0000

    fix: script updated

--- lambda/package_lambda.sh
+++ lambda/package_lambda.sh
@@ -48,7 +48,7 @@ fi
 
 # Instalar dependencias
 echo "⬇️ Instalando dependencias desde $REQUIREMENTS..."
-pip install -t "$LAYER_DIR/python/lib/python3.9/site-packages" -r "$REQUIREMENTS" --no-cache-dir >/dev/null
+pip3 install -t "$LAYER_DIR/python" -r "$REQUIREMENTS" --no-cache-dir >/dev/null
 
 # Limpiar archivos innecesarios
 echo "🧽 Limpiando archivos innecesarios..."
