c789b11bbf398efd641c97af100d2e272e51be82
Edilberto Mario Rodríguez Fontalvo <edimarod@outlook.es>
2025-06-01 19:48:10 +0000

    fix: lambda and step functions fixed

--- lambda.tf
+++ lambda.tf
@@ -23,6 +23,7 @@ resource "aws_iam_role_policy" "lambda_s3_write_access" {
       {
         Effect = "Allow",
         Action = [
+          "s3:GetObject",
           "s3:PutObject"
         ],
         Resource = "arn:aws:s3:::source-data-bucket-6i2caq/raw/*"
