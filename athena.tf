fa82fa3ca3f943d58677d06c1dc68675ee0cde07
Edilberto Mario Rodríguez Fontalvo <edimarod@outlook.es>
2025-06-03 19:57:37 +0000

    feat: resources fixed

--- athena.tf
+++ athena.tf
@@ -16,6 +16,124 @@ resource "aws_glue_catalog_table" "athena_table" {
       name                  = "fires-serde"
       serialization_library = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
     }
+
+    # Columnas definidas correctamente aquí
+    columns {
+      name = "latitude"
+      type = "double"
+    }
+    columns {
+      name = "longitude"
+      type = "double"
+    }
+    columns {
+      name = "population_density"
+      type = "double"
+    }
+    columns {
+      name = "days"
+      type = "int"
+    }
+    columns {
+      name = "wind_speed"
+      type = "double"
+    }
+    columns {
+      name = "vapor_pressure_deficit"
+      type = "double"
+    }
+    columns {
+      name = "vapor_pressure"
+      type = "double"
+    }
+    columns {
+      name = "minimum_temperature"
+      type = "double"
+    }
+    columns {
+      name = "maximum_temperature"
+      type = "double"
+    }
+    columns {
+      name = "snow_water_equivalent"
+      type = "double"
+    }
+    columns {
+      name = "surface_shortwave_radiation"
+      type = "double"
+    }
+    columns {
+      name = "soil_moisture"
+      type = "double"
+    }
+    columns {
+      name = "runoff"
+      type = "double"
+    }
+    columns {
+      name = "precipitation_accumulation"
+      type = "double"
+    }
+    columns {
+      name = "Reference_evapotranspiration"
+      type = "double"
+    }
+    columns {
+      name = "climate_water_deficit"
+      type = "double"
+    }
+    columns {
+      name = "actual_Evapotranspiration"
+      type = "double"
+    }
+    columns {
+      name = "palmer_drought_severity_index"
+      type = "double"
+    }
+    columns {
+      name = "brightness_temperature"
+      type = "double"
+    }
+    columns {
+      name = "scan_fire_size"
+      type = "double"
+    }
+    columns {
+      name = "track_fire_size"
+      type = "double"
+    }
+    columns {
+      name = "confidence"
+      type = "int"
+    }
+    columns {
+      name = "fire_radiative_power"
+      type = "double"
+    }
+    columns {
+      name = "daynight"
+      type = "string"
+    }
+    columns {
+      name = "fire_type"
+      type = "string"
+    }
+    columns {
+      name = "n_pixels_ndvi"
+      type = "int"
+    }
+    columns {
+      name = "ndvi"
+      type = "double"
+    }
+    columns {
+      name = "ndvi_long_term_average"
+      type = "double"
+    }
+    columns {
+      name = "ndvi_anomaly_percent"
+      type = "double"
+    }
   }
 
   lifecycle {
